4ff24a4e301c2842332eff7f786ca28081947ab9
Dinh <31506427+kodliber@users.noreply.github.com>
2018-03-30 21:00:55 +0200

    Ajout des classes DAO depuis le projet SQLite. Adaptation de la classe Article et des classes DAO (definition de la BDD entre autres). Pas encore de fonctionnel.
    Signed-off-by: kodliber <quodliber@gmail.com>

---
+++ app/src/main/java/com/example/a77011_40_15/a1streetart/Dao/ArticleDao.java
@@ -0,0 +1,129 @@
+package com.example.a77011_40_15.a1streetart.Dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.a77011_40_15.a1streetart.Entities.Article;
+import com.example.a77011_40_15.a1streetart.Entities.Articles;
+
+public class ArticleDao {
+    SQLiteDatabase bdd;
+    ArticleSqlite articleSqlite;
+
+//    public ArticleDao (SQLiteDatabase bdd) {this.bdd = bdd}
+
+    public ArticleDao(Context context)
+    {
+        articleSqlite = new ArticleSqlite(context, Constants.NOM_BDD, null, Constants.VERSION);
+    }
+
+    public void openForWrite()
+    {
+        bdd = articleSqlite.getWritableDatabase();
+    }
+
+    public void openForRead()
+    {
+        bdd = articleSqlite.getReadableDatabase();
+    }
+
+    public void close()
+    {
+        bdd.close();
+    }
+
+    public long insertArticle(Article article)
+    {
+        ContentValues content = new ContentValues();
+        content.put(Constants.COL_NAME, article.getName());
+        content.put(Constants.COL_DESC, article.getDescription());
+        return bdd.insert(Constants.TABLE_ARTICLES, null, content);
+    }
+
+    public long updateArticle(Article article)
+    {
+        ContentValues content = new ContentValues();
+        content.put(Constants.COL_NAME, article.getName());
+        content.put(Constants.COL_DESC, article.getDescription());
+        return bdd.update(Constants.TABLE_ARTICLES, content, Constants.COL_ID + " = " + article.getId(), null);
+    }
+
+    public int deleteArticle(String name)
+    {
+        return bdd.delete(Constants.TABLE_ARTICLES, Constants.COL_NAME + " = " + name, null);
+    }
+
+    public int deleteArticle(int id)
+    {
+        return bdd.delete(Constants.TABLE_ARTICLES, Constants.COL_ID + " = " + id, null);
+    }
+
+    public Article getArticle(String name)
+    {
+        Cursor c = bdd.query(Constants.TABLE_ARTICLES, new String[]{
+                        Constants.COL_ID,
+                        Constants.COL_NAME,
+                        Constants.COL_DESC},
+                Constants.COL_NAME + " LIKE \"" + name + "\"",
+                null, null, null, Constants.COL_NAME);
+        return cursorToArticle(c);
+    }
+
+    public long getArticle(int id)
+    {
+        Cursor c = bdd.query(Constants.TABLE_ARTICLES, new String[]{
+                        Constants.COL_ID,
+                        Constants.COL_NAME,
+                        Constants.COL_DESC},
+                Constants.COL_ID + " LIKE \"" + id + "\"",
+                null, null, null, Constants.COL_NAME);
+        return cursorToArticle(c).getId();
+    }
+
+    public Article cursorToArticle(Cursor c)
+    {
+        c.moveToFirst();
+        Article article = null;
+
+        if (c.getCount() > 0) {
+            article = new Article();
+            article.setId(c.getInt(Constants.NUM_COL_ID));
+            article.setName(c.getString(Constants.NUM_COL_NAME));
+            article.setDescription(c.getString(Constants.NUM_COL_DESC));
+            c.close();
+        }
+        return article;
+    }
+
+    public Articles getAllArticles()
+    {
+
+        Cursor c = bdd.query(Constants.TABLE_ARTICLES, new String[]{
+                        Constants.COL_ID,
+                        Constants.COL_NAME,
+                        Constants.COL_DESC},
+                null, null, null, null, Constants.COL_NAME);
+
+        Articles articles = null;
+
+        if (c.getCount() > 0) {
+//            c.moveToFirst();
+            articles = new Articles();
+            Article article;
+            while (c.moveToNext()) {
+                article = new Article();
+                article.setId(c.getInt(0));
+                article.setName(c.getString(1));
+                article.setDescription(c.getString(2));
+
+                articles.add(article);
+            }
+        }
+        close();
+
+        return articles;
+    }
+
+}
